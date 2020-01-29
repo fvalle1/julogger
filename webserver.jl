45e0dc1b63f434b7778aa276022a98d53abe00c0
Filippo Valle <filippo.valle@zoho.com>
2020-01-29 19:05:04 +0100

    initial commit

---
+++ webserver.jl
@@ -0,0 +1,36 @@
+using Pages
+using HTTP
+using JSON
+
+Endpoint("/hello") do request::HTTP.Request
+"Hello world"
+end
+
+Endpoint("/echo") do request::HTTP.Request
+        params = HTTP.queryparams(HTTP.URI(request.target).query)
+        println("Body: $(params)")
+        open("logfile.txt", "a") do io
+            println(io, params["id"])
+        end
+        response = JSON.json(params)
+end
+
+Endpoint("/echo",POST) do request::HTTP.Request
+        data = String(request.body)
+        open("logfile.txt","a") do io
+		println("Parameters: $(data)")
+		println(io, data)
+        end
+        response = JSON.json(Dict(:data => data))
+end
+
+Endpoint("/") do request::HTTP.Request
+    read(joinpath(@__DIR__,"index.html"),String)
+end
+
+open("logfile.txt", "r") do io
+    a = readlines(io)
+    print([parse(Int,x[1]) for x in a])
+end
+
+@async Pages.start(8000);
